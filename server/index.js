66eba457e46d56b60c0c4f4e913e2f494666dd28
Lyfesavers <lyfesaverscoin@gmail.com>
2025-01-10 04:20:32 -0500

    re

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:5001"],
+    origin: ["https://aquads.onrender.com", "http://localhost:5001"],
     methods: ["GET", "POST"],
     credentials: true
   }
