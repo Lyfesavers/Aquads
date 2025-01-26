9d11501bd07524b713d56c46ca92db93eeabe8dd
Lyfesavers <lyfesaverscoin@gmail.com>
2025-01-26 16:11:38 -0500

    fgh

--- src/components/WelcomeModal.js
+++ src/components/WelcomeModal.js
@@ -3,7 +3,7 @@ import './WelcomeModal.css';
 
 const WelcomeModal = ({ username, onClose }) => {
   const handleShare = () => {
-    const tweetText = encodeURIComponent(`🎉 Just joined Aquads! Can't wait to explore amazing services and connect with the community! Check it out! #Aquads #Community\n\nhttps://i.imgur.com/3kCQx6G.png`);
+    const tweetText = encodeURIComponent(`🎉 Just joined Aquads! Can't wait to explore amazing services and connect with the community! Check it out! #Aquads #Community\n\nhttps://aquads.xyz/`);
     window.open(`https://twitter.com/intent/tweet?text=${tweetText}`, '_blank');
   };
 
