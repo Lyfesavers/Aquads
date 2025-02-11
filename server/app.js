524644428b08a82568946aeb463dd27c00d1e4b6
Lyfesavers <lyfesaverscoin@gmail.com>
2025-02-11 13:48:42 -0500

    gvig

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const serviceRoutes = require('./routes/services');
 const serviceReviewRoutes = require('./routes/serviceReviews');
 const bannerAdsRoutes = require('./routes/bannerAds');
 const pointsRoutes = require('./routes/points');
+const bookingsRoutes = require('./routes/bookings');
 
 // Middleware
 const app = express();
@@ -18,5 +19,6 @@ app.use('/api/services', serviceRoutes);
 app.use('/api/service-reviews', serviceReviewRoutes);
 app.use('/api/bannerAds', bannerAdsRoutes);
 app.use('/api/points', pointsRoutes);
+app.use('/api/bookings', bookingsRoutes);
 
 module.exports = app; 
