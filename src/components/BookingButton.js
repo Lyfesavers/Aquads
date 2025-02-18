bf397bc129e8ada301cccf67b36bcc5ae82761f8
Lyfesavers <lyfesaverscoin@gmail.com>
2025-02-18 00:49:35 -0500

    adrfg

--- src/components/BookingButton.js
+++ src/components/BookingButton.js
@@ -13,6 +13,8 @@ const BookingButton = ({ service, currentUser, onBookingCreate, showNotification
 
     try {
       const response = await onBookingCreate(service._id, requirements);
+      console.log('Booking Response:', response);
+      console.log('Seller Data:', response.sellerId);
       
       // Send booking notification to seller
       await emailService.sendBookingNotification(
