daefef8d17a162679490d8a08accc17ddf585326
Lyfesavers <lyfesaverscoin@gmail.com>
2025-05-17 16:51:58 -0400

    ghdgh

--- src/components/CreateJobModal.js
+++ src/components/CreateJobModal.js
@@ -168,6 +168,15 @@ const CreateJobModal = ({ onClose, onCreateJob, job = null }) => {
             />
           </div>
 
+          <div className="mt-4 p-3 bg-indigo-900/50 border border-indigo-600/30 rounded-lg">
+            <p className="text-indigo-300 text-sm flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2h-1V9z" clipRule="evenodd" />
+              </svg>
+              Note: Job postings will automatically expire after 30 days. You can refresh expired jobs from your dashboard to move them back to the top of the listings.
+            </p>
+          </div>
+
           <div className="flex justify-end gap-4">
             <button
               type="button"
@@ -180,7 +189,7 @@ const CreateJobModal = ({ onClose, onCreateJob, job = null }) => {
               type="submit"
               className="px-4 py-2 bg-blue-500 rounded hover:bg-blue-600"
             >
-              Create Job
+              {job ? 'Update Job' : 'Create Job'}
             </button>
           </div>
         </form>
