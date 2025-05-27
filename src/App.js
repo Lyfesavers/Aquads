02109a0d45fa82e36115aad755ebf92b826a8e9b
Lyfesavers <lyfesaverscoin@gmail.com>
2025-05-26 22:41:45 -0400

    ghjghc

--- src/App.js
+++ src/App.js
@@ -45,7 +45,6 @@ import NotificationBell from './components/NotificationBell';
 import logger from './utils/logger';
 import './App.css';
 import FilterControls from './components/FilterControls';
-import Swap from './components/Swap';
 import AquaSwap from './components/AquaSwap';
 
 // Simple debounce function implementation
@@ -3183,7 +3182,7 @@ function App() {
           <Route path="/how-to/:slug" element={<HowTo currentUser={currentUser} />} />
           <Route path="/affiliate" element={<Affiliate />} />
           <Route path="/terms" element={<Terms />} />
-          <Route path="/swap" element={<Swap currentUser={currentUser} showNotification={showNotification} />} />
+          <Route path="/swap" element={<AquaSwap currentUser={currentUser} showNotification={showNotification} />} />
           <Route path="/aquaswap" element={<AquaSwap currentUser={currentUser} showNotification={showNotification} />} />
         </Routes>
       </Router>
