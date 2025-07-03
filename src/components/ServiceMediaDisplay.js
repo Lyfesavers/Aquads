762024d944ed4119e63ec832d305955d3e657cc5
Lyfesavers <lyfesaverscoin@gmail.com>
2025-07-03 13:50:25 -0400

    ndfg

--- src/components/ServiceMediaDisplay.js
+++ src/components/ServiceMediaDisplay.js
@@ -139,7 +139,7 @@ const ServiceMediaDisplay = ({ service, className = "w-full h-48" }) => {
         <ServiceImageComponent 
           src={service.image}
           alt={service.title}
-          className="w-full h-full object-cover"
+          className="w-full h-full object-contain"
         />
         
         {/* Video indicator overlay */}
