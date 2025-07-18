098b8bcd7cc25a872dd04ca90178815774e29676
Lyfesavers <lyfesaverscoin@gmail.com>
2025-07-18 04:18:57 -0400

    fgh

--- server/utils/telegramService.js
+++ server/utils/telegramService.js
@@ -978,7 +978,7 @@ Hi ${username ? `@${username}` : 'there'}! I help you complete Twitter raids and
       message += `💡 Vote on bubbles to earn points!`;
 
       // Get the video file path
-      const videoPath = path.join(__dirname, '../../public/trend.mp4');
+      const videoPath = path.join(__dirname, '../../public/trendnew.mp4');
       const videoExists = fs.existsSync(videoPath);
 
       // Send to the specific group
