f15a344620fbdb4bd6f8570c00a39bfcff379ce7
Lyfesavers <lyfesaverscoin@gmail.com>
2025-08-24 22:57:03 -0400

    dfgd

--- src/App.js
+++ src/App.js
@@ -3018,7 +3018,7 @@ function App() {
                   </div>
                   
                   {/* Bubbles section - keep it as is, remove fixed positioning */}
-                  <div className="relative min-h-screen overflow-hidden pt-7">
+                  <div className="relative min-h-screen overflow-hidden pt-5">
                     {/* Ads */}
                     {getVisibleAds().length > 0 ? (
                       getVisibleAds().map(ad => {
