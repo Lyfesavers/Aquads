1db7f3d85175a15dcb145c4c02152a0a701c2b54
Lyfesavers <lyfesaverscoin@gmail.com>
2025-11-28 19:04:12 -0500

    cbn

--- server/index.js
+++ server/index.js
@@ -472,7 +472,6 @@ app.use('/api/admin', require('./routes/admin'));
 app.use('/api/discount-codes', require('./routes/discountCodes'));
 app.use('/api/skill-tests', require('./routes/skillTests'));
 app.use('/api/workshop', require('./routes/workshop'));
-app.use('/api/telegram', require('./routes/telegram'));
 
 
 // Special route for blog sharing metadata (outside the API namespace)
