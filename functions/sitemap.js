21176232cfaa3d7a39bc3e9f18d578fc572c387b
Lyfesavers <lyfesaverscoin@gmail.com>
2025-12-22 04:57:17 -0500

    mgh

--- functions/sitemap.js
+++ functions/sitemap.js
@@ -1,62 +1,106 @@
 exports.handler = async (event, context) => {
+  const today = new Date().toISOString().split('T')[0];
+  
   const sitemapXML = `<?xml version="1.0" encoding="UTF-8"?>
-<!-- Updated: 2025-06-12 - Static sitemap for Aquads -->
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://www.aquads.xyz/</loc>
-    <lastmod>2025-06-12</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>daily</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>https://www.aquads.xyz/marketplace</loc>
-    <lastmod>2025-06-12</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
-            <loc>https://www.aquads.xyz/learn</loc>
-    <lastmod>2025-06-12</lastmod>
+    <loc>https://www.aquads.xyz/learn</loc>
+    <lastmod>${today}</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://www.aquads.xyz/dashboard</loc>
-    <lastmod>2025-06-12</lastmod>
+    <loc>https://www.aquads.xyz/aquaswap</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://www.aquads.xyz/games</loc>
+    <lastmod>${today}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
+    <loc>https://www.aquads.xyz/games/horse-racing</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>https://www.aquads.xyz/games/dots-and-boxes</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <url>
     <loc>https://www.aquads.xyz/whitepaper</loc>
-    <lastmod>2025-06-12</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>
   <url>
     <loc>https://www.aquads.xyz/affiliate</loc>
-    <lastmod>2025-06-12</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>https://www.aquads.xyz/aquafi</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://www.aquads.xyz/partner-rewards</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.7</priority>
+  </url>
+  <url>
+    <loc>https://www.aquads.xyz/why-list</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.6</priority>
+  </url>
+  <url>
+    <loc>https://www.aquads.xyz/freelancer-benefits</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.6</priority>
   </url>
   <url>
     <loc>https://www.aquads.xyz/terms</loc>
-    <lastmod>2025-06-12</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>monthly</changefreq>
-    <priority>0.5</priority>
+    <priority>0.4</priority>
   </url>
   <url>
-    <loc>https://www.aquads.xyz/service</loc>
-    <lastmod>2025-06-12</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.7</priority>
+    <loc>https://www.aquads.xyz/privacy-policy</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.4</priority>
   </url>
 </urlset>`;
 
   return {
     statusCode: 200,
     headers: {
-      'Content-Type': 'application/xml',
-      'Cache-Control': 'public, max-age=3600'
+      'Content-Type': 'application/xml; charset=utf-8',
+      'Cache-Control': 'public, max-age=3600',
+      'X-Robots-Tag': 'noindex'
     },
     body: sitemapXML
   };
